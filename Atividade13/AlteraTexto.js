d988bc440dda4a62bd96b6afc888dd18863711b1
lucas-henrique-cardoso-aparecida <78798597+lucas-henrique-cardoso-aparecida@users.noreply.github.com>
2022-05-30 02:37:23 -0300

    Add files via upload

---
+++ Atividade13/AlteraTexto.js
@@ -0,0 +1,10 @@
+
+const toUpper = () =>{
+      const input = document.body.querySelector('#inputText');
+      input.value = input.value.toUpperCase();
+};
+
+const toLower = () =>{
+      const input = document.body.querySelector('#inputText');
+      input.value = input.value.toLowerCase();
+};
